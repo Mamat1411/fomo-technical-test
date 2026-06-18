32308adcc37c6d00d227f4d9a42b348e326fd051
Mamat1411 <muhammadmujahid14@gmail.com>
2026-06-18 22:41:43 +0700

    [Fix] Resolve no column named ID on users table migration

--- task-1-online-store/database/migrations/0001_01_01_000000_create_users_table.php
+++ task-1-online-store/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid()->primary()->default(Str::uuid());
+            $table->uuid('id')->primary()->default(Str::uuid());
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
