becd0e8e7e2a8f0e4ea2fb44c9fec2a575c33068
Mamat1411 <muhammadmujahid14@gmail.com>
2026-06-18 20:46:43 +0700

    [Build] Install Laravel API

---
+++ task-1-online-store/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/up', function () {
+    return response()->json([
+        "status" => 200,
+        "message" => "Application is healthy"
+    ]);
+});
